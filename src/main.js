b759cce72bd1cdb2064887254f5dd6047d9fcb07
bubblecigar <45429573+bubblecigar@users.noreply.github.com>
2019-09-10 14:34:58 +0800

    add firebase service

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from '@/firebaseConfig.js'
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD335CxxSSB7H6yXZdMJ6JTS9OXL8vQcY",
+  authDomain: "vue-cli-3-4295f.firebaseapp.com",
+  databaseURL: "https://vue-cli-3-4295f.firebaseio.com",
+  projectId: "vue-cli-3-4295f",
+  storageBucket: "vue-cli-3-4295f.appspot.com",
+  messagingSenderId: "950038836578",
+  appId: "1:950038836578:web:a79bd367f7f5b88dd1e83b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
 
 Vue.config.productionTip = false;
 
